bbb366705c13e2376dd380c8c81661a73a73a59d
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2022-06-21 14:04:36 -0300

    Entrega exercícios.

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Semana 9 victor/script.js
@@ -0,0 +1,33 @@
+function calcule(){
+    var number = document.getElementById("valor").value;
+    centena = Number(number[0]);
+    if (number >= 100 && number != ""){
+        if (centena % 2 == 0){
+            resultado = "A centena é par";
+        } else {
+            resultado = "A centena é ímpar";
+        }
+    } else{
+        resultado = "Digite um número válido"
+    }
+    document.getElementById("resultado").innerHTML = resultado 
+}
+
+function soma(){
+    var number = document.getElementById("valor1").value
+    var resultado = 0
+    for(var i = 0; i!= number.length; i++){
+        numero = parseInt(number[i]);
+        resultado += numero
+    }
+    document.getElementById("resultado1").innerHTML = resultado
+}
+
+function nomes(){
+    var nome1 = document.getElementById("nome1").value
+    var nome2 = document.getElementById("nome2").value
+    var nome3 = document.getElementById("nome3").value
+    var nomes = [nome1, nome2, nome3];
+    nomes = nomes.sort();
+    document.getElementById("resultado2").innerHTML = nomes
+}
